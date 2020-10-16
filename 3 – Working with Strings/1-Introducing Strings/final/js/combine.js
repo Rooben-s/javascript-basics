9087b33755a0f6de09b28d5e4b48f2ee2ad4a855
Rooben Subramanen <djrooben@gmail.com>
2020-10-16 22:33:28 +1100

    solution

---
+++ 3 – Working with Strings/1-Introducing Strings/final/js/combine.js
@@ -0,0 +1,25 @@
+const name = prompt("What is your name?");
+let message = "Hello, " + name + ".Welcome to my music site. " ;
+message += "I'm so happy that you came by to visit,";
+message += name;
+message += ". Feel free to come again and listen to more music!";
+
+console.log(message);
+
+
+
+
+
+
+
+
+// Finally, convert the string stored in role to uppercase letters. The final msg string should look similar to this: "Carlos Salgado: DEVELOPER"
+
+let firstName = 'rooben';
+let lastName = 'subramanen';
+let role = 'developer';
+
+let msg = firstName + " " + lastName + ": " + role.toUpperCase();
+
+
+console.log(msg);
